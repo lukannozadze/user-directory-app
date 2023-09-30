a0eb2d139beaa344283c793f977f94a04cbf4050
Luka Nozadze <l.nozadze@oppa.io>
2023-09-30 12:26:44 +0400

    setting up project dependencies:Installed Node.js modules and configured Tailwind CSS for the project.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+<div>
+  <h1 className='text-red-500'>I'm Starting Project For Smart Solutions AI</h1>
+</div>
+  )
+}
+
+export default App
